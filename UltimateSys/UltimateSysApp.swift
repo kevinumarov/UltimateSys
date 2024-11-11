4aa25945f568ffd1e8a2e49acee8f731cb845054
kevinumarov <keulifeinfluencer@gmail.com>
2024-11-11 20:45:37 +0900

    Initial Commit

---
+++ UltimateSys/UltimateSysApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateSysApp.swift
+//  UltimateSys
+//
+//  Created by Kevin Umarov on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UltimateSysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
